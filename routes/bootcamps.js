10d437b3c764d8c23a31e9fd2abe48ec52ff2012
Shashaank <shashaankramesh@gmail.com>
2020-08-15 11:17:39 +0530

    Adding controller methods & logger  Middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,25 +1,11 @@
 const express = require('express');
+const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp } = require('../controllers/bootcamps')
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-});
+router.route('/').get(getBootcamps).post(createBootcamp);
 
-router.get('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Show bootcamp data for ${req.params.id}` });
-});
-
-router.post('/', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Create new bootcamps' });
-});
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
 //Routes files
 const bootcamps = require('./routes/bootcamps');
 
@@ -9,11 +9,15 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 //Mount Routers
 app.use('/api/v1/bootcamps', bootcamps );
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
