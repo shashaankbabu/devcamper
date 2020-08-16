f90cccabb30791e0927b9872668c786c52d8e14b
Shashaank <shashaankramesh@gmail.com>
2020-08-16 13:07:24 +0530

    Pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,15 +2,78 @@ const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
+const { remove } = require('../models/Bootcamp');
 
 // @desc  Get all bootcamps
 // @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-        const bootcamps = await Bootcamp.find();
+        let query;
+
+        //Copy req.query
+        const reqQuery = { ...req.query };
+
+        //Field to exclude
+        const removeFields = ['select', 'sort', 'page', 'limit'];
+
+        //Loop over remove fields and delete from reqQuery
+        removeFields.forEach(param => delete reqQuery[param]);
+
+        //Copy query string
+        let queryStr = JSON.stringify(reqQuery);
+
+        //Create operators ($gt, $gte, etc)
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|in)\b/g, match => `$${match}`);
+        
+        //Finding resource
+        query = Bootcamp.find(JSON.parse(queryStr));
+
+        //Select fields
+        if(req.query.select) {
+            const fields = req.query.select.split(',').join(' ');
+            query = query.select(fields);
+        }
+
+        //Sort
+        if(req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+        //Pagination
+        const page = parseInt(req.query.page, 10) || 1;
+        const limit = parseInt(req.query.limit, 10) || 25;
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        const total = await Bootcamp.countDocuments();
+
+        query = query.skip(startIndex).limit(limit);
+    
+        //Executing query
+        const bootcamps = await query;
+
+        //Pagiantion result
+        const pagination = {};
+        if(endIndex < total) {
+            pagination.next = {
+                page: page + 1,
+                limit
+            }
+        }
+
+        if(startIndex > 0) {
+            pagination.prev = {
+                page: page - 1,
+                limit
+            }
+        }
+
         res.status(200).json({
             success: true,
             count: bootcamps.length,
+            pagination,
             data: bootcamps
         });
 });
